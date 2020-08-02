3145cb8018759287798fa3a01ed16f22f6f280d1
pratiman-91 <pratiman_patel@hotmail.com>
2020-08-02 12:12:03 +0530

    copy button test 2

--- assets/js/copy-code-button.js
+++ assets/js/copy-code-button.js
@@ -1,3 +1,17 @@
+document.querySelectorAll('pre > code').forEach(function (codeBlock) {
+    var button = document.createElement('button');
+    button.className = 'copy-code-button';
+    button.type = 'button';
+    button.innerText = 'Copy';
+  
+    var pre = codeBlock.parentNode;
+    if (pre.parentNode.classList.contains('highlight')) {
+        var highlight = pre.parentNode;
+        highlight.parentNode.insertBefore(button, highlight);
+    } else {
+        pre.parentNode.insertBefore(button, pre);
+    }
+  });
 function addCopyButtons(clipboard) {
     document.querySelectorAll('pre > code').forEach(function (codeBlock) {
         var button = document.createElement('button');
